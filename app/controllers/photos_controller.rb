f07f3860832f8965a82ca9362022a8a27b10b22c
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-07-07 14:04:05 +0600

    photos controller added

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,17 @@
+class PhotosController <ApplicationController
+
+    def create 
+        @room = Room.find(params[:room_id])
+
+        if params[:images]
+            params[:images].each do |img|
+                @room.photos.create(image:img)
+
+            end
+            @photos = @room.photos
+            flash[:notice] = "Photos successfully saved!"
+            redirect_back(fallback_location:request.referer)
+     end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,6 @@ resources :rooms, except:[:edit] do
     get 'amenities'
     get 'location'
   end
+   resources :photos, only: [:create, :destroy]
 end
 end
